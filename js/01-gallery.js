f219a64af646ada14d68009c7a261a53f657a97a
DmitrovD <92047119+DmitrovD@users.noreply.github.com>
2022-04-25 08:15:06 +0300

    Update 01-gallery.js
    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallaryContainer = document.querySelector('.gallery');
+const gallaryImgMarkup = galleryItems.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    })
+    .join(``);
+
+gallaryContainer.insertAdjacentHTML('beforeend', gallaryImgMarkup);
+
+gallaryContainer.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    const modal = basicLightbox.create(`<img src="${e.target.dataset.source}" alt="${event.target.alt}">`);
+    modal.show();
+
+    if(modal.visible()) {
+        window.addEventListener(`keydown`, (event) => {
+            console.log(event);
+            if(event.code === `Escape`) {
+                modal.close();
+            };
+        });
+    };
+});
