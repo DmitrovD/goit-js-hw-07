9f076be8db8f697c27e22cef09310c4051cf166b
DmitrovD <92047119+DmitrovD@users.noreply.github.com>
2022-04-25 08:29:26 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const gallaryContainer = document.querySelector('.gallery');
 const gallaryImgMarkup = galleryItems.map(({ preview, original, description }) => {
